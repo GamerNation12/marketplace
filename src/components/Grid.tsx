80ca7e24b055003cc9e21fd9d3cd80330fd3ccac
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-03-11 18:29:24 -0700

    Update Grid.tsx

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -22,10 +22,10 @@ import { TopBarContent } from "./TabBar";
 import Tooltip from "./Tooltip";
 
 // --- GITHUB TOKEN VIP PASS ---
-const GITHUB_TOKEN = "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"; 
+const GITHUB_TOKEN = "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";
 
 const originalFetch = window.fetch;
-window.fetch = async function (resource, options) {
+window.fetch = async (resource, options) => {
   if (typeof resource === "string" && resource.includes("api.github.com")) {
     options = options || {};
     options.headers = options.headers || {};
